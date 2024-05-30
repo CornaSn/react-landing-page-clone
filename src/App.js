a348d4b6d1d4a24f8d15b9e1ab01df3eb4dd8b08
CornaSn <cornelia.schenk@hotmail.com>
2024-05-30 16:41:05 +0200

    Modify styling

--- src/App.js
+++ src/App.js
@@ -195,7 +195,13 @@ export default function App() {
               <MagicSpellSvg />
             </div>
             <picture className={styles.picturesMagicSpell}>
-              <img id="#magic-icon" src="img/magic-icon.jpg" alt="magic.icon" />
+              <img
+                className={styles.picturesMagicSpellOne}
+                id="#magic-icon"
+                src="img/magic-icon.jpg"
+                alt="magic.icon"
+                height="400rem"
+              />
               {/* <img id="#magic-copy" src="img/magic-copy.jpg" alt="magic-copy" />
               <img id="#magic-img" src="img/magic-image.jpg" alt="magic-img" /> */}
             </picture>
@@ -211,23 +217,19 @@ export default function App() {
               <p>Be more creative with smarter design tools.</p>
             </hgroup>
             <div className={styles.apps}>
-              <div className={styles.automator}>
-                <MultiColorSvg />
-                <h2>Automator</h2>
-                <p>There's an automation for that</p>
-                <a href="/">Learn more</a>
-                {/* <div className={styles.appPreview}>
-                  <AutomatorSvg />
-                </div> */}
-              </div>
-              <div className={styles.prototyper}>
-                <PrototyperIconSvg />
-                <h2>Prototyper</h2>
-                <p>Powerful prototyping with code</p>
-                <a href="/">Learn more</a>
-                {/* <div className={styles.appPreview}>
-                  <PrototyperSvg />
-                </div> */}
+              <div className={styles.appContainer}>
+                <div className={styles.automator}>
+                  <MultiColorSvg />
+                  <h2>Automator</h2>
+                  <p>There's an automation for that</p>
+                  <a href="/">Learn more</a>
+                </div>
+                <div className={styles.prototyper}>
+                  <PrototyperIconSvg />
+                  <h2>Prototyper</h2>
+                  <p>Powerful prototyping with code</p>
+                  <a href="/">Learn more</a>
+                </div>
               </div>
               <div className={styles.designers}>
                 <p>Used by designers on these teams:</p>
@@ -258,7 +260,7 @@ export default function App() {
                   <InstacartSvg />
                 </li>
                 <li>
-                  <RedditSvg /> align-items: center;
+                  <RedditSvg />
                 </li>
               </ul>
             </div>
@@ -271,21 +273,22 @@ export default function App() {
               <PricingSvg />
               <h1 className={styles.iconHeading}>Pricing</h1>
             </hgroup>
-          </div>
-          <div className={styles.pricing}>
-            <PublicbetaSvg />
-            <h3>Public beta</h3>
-            <br />
-            <strong>Free</strong>
-            <p>1 license, unlimited use.</p>
-            <ul className={styles.pricingList}>
-              <li>Early access</li>
-              <li>All magic spells</li>
-              <li>All future updates</li>
-            </ul>
-            <button className={styles.buttonTry}>
-              <a href="/">Try for free</a>
-            </button>
+
+            <div className={styles.pricing}>
+              <PublicbetaSvg />
+              <h3>Public beta</h3>
+              <br />
+              <strong>Free</strong>
+              <p>1 license, unlimited use.</p>
+              <ul className={styles.pricingList}>
+                <li>Early access</li>
+                <li>All magic spells</li>
+                <li>All future updates</li>
+              </ul>
+              <button className={styles.buttonTry}>
+                <a href="/">Try for free</a>
+              </button>
+            </div>
           </div>
         </section>
         <section className={styles.sectionDarkpurple}>
@@ -306,7 +309,7 @@ export default function App() {
           <footer className={styles.header}>
             <a href="/" className={styles.logo}>
               <LogoSvg />
-              <h1>Magician</h1>
+              <h>Magician</h>
             </a>
             <nav className={styles.navigation}>
               <ul>
